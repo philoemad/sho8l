55edf0c58c8c9bf3d79a1719b4c225442135c14e
Abdulrhman Asim <Abdulrhman.asim@gmail.com>
2019-12-26 19:53:02 +0200

    Added functions needed for Adding Friends, and Made minor adjustments. PLEASE READ THE TEXT FILE INCLUDED

--- user.java
+++ user.java
@@ -4,62 +4,54 @@
  * and open the template in the editor.
  */
 package user;
+
 import java.sql.DriverManager;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.*;
 import java.util.Vector;
 
-/**
- *
- * @author philo
- */
-public class user extends userdbmodel{
+public class user extends userdbmodel {
 
-  public String username;
+  private static user users;
+  private String username;
+  private String password;
+  private String country;
+  private String gender;
+  private String groupname;
+  private Vector myuser;
+  private Vector mymessage;
+  private Vector mygroup_interface;
+  private Vector mypost;
 
-    public String password;
-
-  public String country;
-
-  public String gender;
-
-  public String groupname;
- private static user users;
-
-    public Vector  myuser;
-    public Vector  mymessage;
- 
-    public Vector  mygroup_interface;
-   
-    public Vector  mypost;
-    
- /* public Integer addfrienduser( userID) {
-  return null;
+  /*
+   * public Integer addfrienduser( userID) { return null; } public void login(
+   * username, password) { }
+   */
+  protected user() throws SQLException {
+    System.out.print("class user");
   }
 
-  public void login( username,  password) {
-  }*/
-  protected user()  throws SQLException {
-   System.out.print("class user");}
-  public void register(int id,String names,String usernames,String passwords,String genders, String countrys) throws SQLException {
-      this.username=usernames;
-      this.password=passwords;
-      this.gender=genders;
-      this.country=countrys;
-      userdbmodel regist=new userdbmodel();
-      regist.save( id, names, usernames, passwords, genders,  countrys);
-      
-      
+  public void register(String name, String username, String password, String gender, String country)
+      throws SQLException {
+    this.username = usernames;
+    this.password = passwords;
+    this.gender = genders;
+    this.country = countrys;
+    userdbmodel regist = new userdbmodel();
+    regist.save(name, username, password, gender, country);
+
   }
 
   public void validate() {
   }
-  public static  user returnuser()  throws SQLException {
-      if(users==null){
-            return  new user();}
-      return users;
-  
+
+  public static user returnuser() throws SQLException {
+    if (users == null) {
+      return new user();
+    }
+    return users;
+
   }
 
 }
--- userdbmodel.java
+++ userdbmodel.java
@@ -1,69 +1,145 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package user;
-import java.sql.DriverManager;
-import java.sql.Connection;
-import java.sql.SQLException;
+
+import java.util.Vector;
 import java.sql.*;
-/**2
- *
- * @author philo
- */
+
 public class userdbmodel {
-     static final String DB_URL =
-      "jdbc:mysql://localhost:3306/social_network";
-   static final String DB_DRV =
-      "com.mysql.jdbc.Driver";
+   
+   static final String DB_URL = "jdbc:mysql://localhost:3306/social_network";
+   static final String DB_DRV = "com.mysql.jdbc.Driver";
    static final String DB_USER = "root";
    static final String DB_PASSWD = "123456789";
-       Connection connection = null;
-      Statement statement = null;
-      ResultSet resultSet = null;
-    public userdbmodel() throws SQLException{
-      
-      
+   Connection connection = null;
+   Statement statement = null;
+   ResultSet resultSet = null;
 
-      try{
-         connection=DriverManager.getConnection
-            (DB_URL,DB_USER,DB_PASSWD);
-         statement=connection.createStatement();
-         resultSet=statement.executeQuery
-            ("SELECT * FROM user");
-         while(resultSet.next()){
-            System.out.printf("%s\t%s\t%s\t%f\n",
-            resultSet.getString(1),
-            resultSet.getString(2),
-            resultSet.getString(3),
-            resultSet.getFloat(4));
+
+   public userdbmodel() throws SQLException {
+
+      try {
+         connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWD);
+         statement = connection.createStatement();
+         resultSet = statement.executeQuery("SELECT * FROM user");
+         while (resultSet.next()) {
+            System.out.printf("%s\t%s\t%s\t%f\n", resultSet.getString(1), resultSet.getString(2),
+                  resultSet.getString(3), resultSet.getFloat(4));
          }
 
-      }catch(SQLException ex){
-      }/*finally{
-         try {
-            resultSet.close();
-            statement.close();
-            connection.close();
-         } catch (SQLException ex) {
-         }
-      }*/
-     
+      } catch (SQLException ex) {
+      }
+
+   }
+
+   public void save(String name, String username, String password, String gender, String country) throws SQLException {
+      PreparedStatement c = connection.prepareStatement("Insert into user values(?,?,?,?,?)");
+
+      c.setString(1, name);
+      c.setString(2, username);
+      c.setString(3, password);
+      c.setString(4, gender);
+      c.setString(5, country);
+      int i = c.executeUpdate();
+      System.out.print("record inserted" + i);
+
+   }
+   public void login(String name, String password) throws SQLException {
+      PreparedStatement c = connection.prepareStatement("SELECT * from user WHERE username = ? and password = ?)");
+
+      c.setString(1, name);
+      c.setString(2, password);
+      ResultSet i = c.executeQuery();
+      if(i.next())
+      {
+         username = i.getString("username");
+         password = i.getString("password");
+         country = i.getString("country");
+         name = i.getString("name");
+         gender = i.getString("gender");
+      }
+      System.out.print("record inserted" + i);
+
+   }
+
+   public void makeRequest(String sender, String receiver) throws SQLException {
+
+      PreparedStatement c = connection.prepareStatement("INSERT INTO requests(sender, receiver) VALUES(?,?)");
+
+      c.setString(1, sender);
+      c.setString(2, receiver);
+      int i = c.executeUpdate();
+      System.out.print("record inserted" + i);
+
+   }
+
+
+
+   public void addFriend(String first, String second) throws SQLException {
+
+      PreparedStatement c = connection.prepareStatement("INSERT INTO friends(first, second) VALUES(?,?)");
+      PreparedStatement d = connection.prepareStatement("DELETE FROM requests WHERE (sender = ? AND receiver = ?) OR (sender = ? AND receiver = ?)");
+
       
-}
-    public void save(int id,String names,String usernames,String passwords,String genders, String countrys) throws SQLException{
-        PreparedStatement c=connection.prepareStatement("Insert into user values(?,?,?,?,?,?)");
+      d.setString(1, first);
+      d.setString(2, second);
+      d.setString(3, second);
+      d.setString(4, first);
       
-        c.setInt(1, id);
-        c.setString(2, names);
-        c.setString(3, usernames);
-        c.setString(4,passwords);
-         c.setString(5,genders);
-          c.setString(6,countrys);
-        int i=c.executeUpdate();
-        System.out.print("record inserted"+i);
-    
-    }
-    
+      c.setString(1, first);
+      c.setString(2, second);
+      int i = c.executeUpdate();
+      d.executeUpdate();
+      System.out.print("record inserted" + i);
+
+
+   }
+
+   public void declineRequest(String first, String second) throws SQLException {
+
+      PreparedStatement d = connection.prepareStatement("DELETE FROM requests WHERE (sender = ? AND receiver = ?) OR (sender = ? AND receiver = ?)");
+
+      
+      d.setString(1, first);
+      d.setString(2, second);
+      d.setString(3, second);
+      d.setString(4, first);
+      
+      int i = d.executeUpdate();
+      System.out.print("record inserted" + i);
+
+
+   }
+
+   public Vector<String> getRequests(String username) throws SQLException {
+
+      PreparedStatement c = connection.prepareStatement("SELECT sender FROM requests where receiver = ?");
+
+      Vector<String> req = new Vector<String>();
+
+      c.setString(1, username);
+      int i = c.executeQuery();
+
+      while (resultSet.next()) {
+         req.add(resultSet.username);
+      }
+
+      return req;
+   }
+
+   public Vector<String> searchUsers(String uname) {
+      
+      PreparedStatement c = connection.prepareStatement("SELECT username FROM user where username = ?");
+
+      Vector<String> users = new Vector<String>();
+
+      c.setString(1, uname);
+      resultSet = c.executeQuery();
+      while (resultSet.next()) {
+         users.add(resultSet.username);
+      }
+
+      return users;
+
+   }
+
 }
